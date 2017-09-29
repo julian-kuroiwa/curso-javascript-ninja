c4aee5f15e410a0c75d09f20ef37388adde6c337
Julian Kuroiwa <julian.kuroiwa@gmail.com>
2017-09-29 17:37:33 -0300

    challenge-08 incomplete

--- challenge-08/challenge-08.js
+++ challenge-08/challenge-08.js
@@ -3,38 +3,49 @@ Declare uma variável chamada `sum` e atribua a ela uma função chamada
 `calculateSum`. A função deve receber dois parâmetros e retornar a soma
 desses parâmetros.
 */
-// ?
+var sum = function calculateSum(x, y) {
+    return x + y;
+}
 
 /*
 Invoque a função criada acima, passando dois números que serão somados, e mostre
 o resultado no console, com a frase:
 "A soma de [VALOR 1] e [VALOR2] é igual a [RESULTADO]."
 */
-// ?
+
+var num1 = 4;
+var num2 = 9;
+
+console.log("A soma de " + num1 + " e " + num2 + " é igual a " + sum(num1, num2) + ".");
+
 
 /*
 Mostre no console o nome da função criada acima, com a frase:
 "O nome da função que faz a soma é [NOME DA FUNÇÃO]."
 */
-// ?
+console.log("O nome da função que faz a soma é " + sum.name + ".");
 
 /*
 Crie uma função literal chamada `showName`. Essa função deve retornar o
 seu nome.
 */
-// ?
+function showName() {
+    return "Julian";
+}
 
 /*
 Declare uma variável chamada `varShowName` que recebe a função criada acima.
 */
-// ?
+
+var varShowName = showName;
 
 /*
 Usando a variável criada acima, mostre no console o nome e o retorno da função
 atribuída a ela, com a seguinte frase:
 "A função [NOME DA FUNÇÃO] retorna [RETORNO DA FUNÇÃO]."
 */
-// ?
+
+console.log("A função " + varShowName.name + " retorna " + varShowName() + ".");
 
 /*
 Crie uma função literal chamada `calculator`, que funcione assim:
@@ -48,7 +59,18 @@ vai efetuar. Será uma string com os valores `+`, `-`, `*`, `/` ou `%`;
   - Se o operador não for válido, retornar a frase:
   "Operação inválida."
 */
-// ?
+
+function calculator(operator) {
+    return function(x, y) {
+        if (operator !== '+' || operator !== '-' || operator !== '*' || operator !== '/' || operator !== '%') {
+            return "Operação inválida.";
+        }
+
+
+
+        return "Resultado da operação: [NUMERO1] [OPERADOR] [NUMERO2] = [RESULTADO].";
+    }
+}
 
 /*
 Declare uma variável chamada `sum`, que receberá a função acima, passando como
