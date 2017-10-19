6f4178fb3336256c721b24d8a2be23045febbaa5
Julian Kuroiwa <julian.kuroiwa@gmail.com>
2017-10-19 21:55:15 -0200

    challenge-21

--- challenge-21/challenge-21.js
+++ challenge-21/challenge-21.js
@@ -1,4 +1,6 @@
-/*
+(function(win, doc) {
+
+    /*
 O desafio de hoje será um pequeno projeto: um cronômetro!
 As regras para criação do cronômetro são as seguintes:
 1. Crie um arquivo index.html e adicione esse script a ele;
@@ -14,4 +16,33 @@ Utilize o atributo data-js para nomear o campo e os botões. Você pode
 usar o nome que achar melhor, desde que ele seja semântico, ou seja, o nome
 dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
 */
-// ?
+
+    var $input = doc.querySelector("[data-js='input']");
+    var $start = doc.querySelector("[data-js='start']");
+    var $stop = doc.querySelector("[data-js='stop']");
+    var $reset = doc.querySelector("[data-js='reset']");
+    var counter = $input.value;
+
+    $start.addEventListener('click', function() {
+        startChrono();
+
+    }, false);
+
+    $stop.addEventListener('click', function() {
+        clearInterval(counter);
+    }, false);
+
+    $reset.addEventListener('click', function() {
+        clearInterval(counter);
+        counter = 0;
+        $input.value = counter;
+    }, false);
+
+    function startChrono() {
+
+        $input.value = counter++;
+        setTimeout(startChrono, 1000);
+    }
+
+
+})(window, document);
